933ae4f167b2bf86fc2a731da44d4d54e9de6ba8
jhk01007 <jhk001007@naver.com>
2025-10-22 15:40:38 +0900

    feat: 콘솔 입력 기능 구현
    - 차 이름 및 라운드 횟수 입력
    - 다양한 출력 방법이 존재할 수 있기 때문에 입력 기능을 다형성을 고려하여 구현

---
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleInputView implements InputView{
+    private static final String CAR_NAME_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ROUND_COUNT_GUIDE = "시도할 횟수는 몇 회인가요?";
+
+    @Override
+    public String readCarNames() {
+        System.out.println(CAR_NAME_GUIDE);
+        return Console.readLine();
+    }
+
+    @Override
+    public String readRoundCount() {
+        System.out.println(ROUND_COUNT_GUIDE);
+        return Console.readLine();
+    }
+}
